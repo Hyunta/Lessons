bd36f89800a124a9ea7218f6e5eff5e18e75a63f
Hyunta <mohai2618@naver.com>
2021-04-14 17:30:05 +0900

    210414 Java 공부

---
+++ Java/inflearn_jsp/pjt/java_prac/src/java_prac/MainClass.java
@@ -0,0 +1,14 @@
+package java_prac;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		int i = 10;
+		int j;
+		j = 20;
+		
+		System.out.println("i+j= "+(i+j));
+		
+	}
+
+}
