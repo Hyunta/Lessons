6ef7fb39ddd2bbce4347ca2bb9c4fabb8bfb283d
Hyunta <mohai2618@naver.com>
2021-04-27 18:28:59 +0900

    210427 Java 생성자 및 패키지

---
+++ Java/inflearn_jsp/pjt/constructor/src/constructor/MainClass.java
@@ -0,0 +1,9 @@
+package constructor;
+
+public class MainClass {
+	public static void main(String[] args) {
+		
+		ObjectClass obj1 = new ObjectClass();
+	}
+
+}
---
+++ Java/inflearn_jsp/pjt/constructor/src/constructor/ObjectClass.java
@@ -0,0 +1,11 @@
+package constructor;
+
+public class ObjectClass {
+	
+	// Constructor 가 정의되어 있지 않아도 적당히 만든다.
+	
+	public ObjectClass() {
+		System.out.println("__ObjectClass()__");
+	}
+
+}
