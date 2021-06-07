9e8ee371e4f8412a9c4d24c6e8ecf76d8fc16ed3
Hyunta <mohai2618@naver.com>
2021-06-07 17:32:45 +0900

    210607 ComponentScan 대상

--- Java/inflearn_스프링 핵심원리/core/src/main/java/hello/core/AutoAppConfig.java
+++ Java/inflearn_스프링 핵심원리/core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
+//
+//        지정하지 않으면 클래스의 프로젝트 최상단 --> com.hello 부터 확인
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
