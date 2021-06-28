8b1125a082ed97c557ef173353f901619f384c3e
Hyunta <mohai2618@naver.com>
2021-06-28 17:07:20 +0900

    210628 스프링 MVC 메서드 지정

--- Java/inflearn_스프링 웹 MVC/servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ Java/inflearn_스프링 웹 MVC/servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -5,6 +5,7 @@ import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -15,12 +16,12 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping("/save")
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,
@@ -33,7 +34,7 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET)
     public String members(Model model) {
 
         List<Member> members = memberRepository.findAll();
