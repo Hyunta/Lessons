59bc639cf776d25fbc84113a61afe3cace2fefff
Hyunta <mohai2618@naver.com>
2021-07-06 16:18:54 +0900

    210706 BeanValidation Object Error 처리

--- Java/inflearn_스프링 웹 MVC1&2/springmvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Java/inflearn_스프링 웹 MVC1&2/springmvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,14 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10,000원이 넘어야합니다.")
 public class Item {
 
     private Long id;
