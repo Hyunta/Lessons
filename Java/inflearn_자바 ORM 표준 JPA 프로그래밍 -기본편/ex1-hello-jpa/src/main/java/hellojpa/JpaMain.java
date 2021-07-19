2f139e594160ee474c6aa58527fef722b5b97d37
Hyunta <mohai2618@naver.com>
2021-07-19 17:17:46 +0900

    feat: 210719 준영속 상태

--- Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,10 +18,12 @@ public class JpaMain {
         try {
 
             //영속
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
 
-            em.flush();
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("============================");
             tx.commit();
