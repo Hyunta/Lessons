56797925df11830e4c7cbfadb0db016a08c05f4a
Hyunta <mohai2618@naver.com>
2021-07-23 16:59:17 +0900

    feat: 1:1 양방향 매핑

--- Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/Locker.java
+++ Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/Locker.java
@@ -3,6 +3,7 @@ package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Locker {
@@ -12,4 +13,7 @@ public class Locker {
     private Long id;
 
     private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
 }
