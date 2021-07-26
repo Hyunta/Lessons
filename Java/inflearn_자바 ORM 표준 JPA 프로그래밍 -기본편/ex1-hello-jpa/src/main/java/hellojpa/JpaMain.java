337a912c59d10015816c035c5f3b1ecf77e486f9
Hyunta <mohai2618@naver.com>
2021-07-26 17:08:42 +0900

    feat: Set InheritanceType to SingleTable

--- Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -17,19 +18,14 @@ public class JpaMain {
 
         try {
 
-            Movie movie = new Movie();
-            movie.setDirector("aaaa");
-            movie.setActor("bbbb");
-            movie.setName("바람");
-            movie.setPrice(10000);
-
-            em.persist(movie);
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
+            em.persist(member);
 
             em.flush();
             em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
