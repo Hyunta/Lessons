67ed879c2acecc552648b8f6e24b3c242dc8f932
Hyunta <mohai2618@naver.com>
2021-07-29 18:10:20 +0900

    feat: 즉시로딩과 지연로딩

--- Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,18 +18,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
-            
+
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
+
             em.flush();
             em.clear();
 
-            Member m = em.getReference(Member.class, member.getId());
-            System.out.println("mrefer = " + m.getClass()); // proxy
-            Hibernate.initialize(m);
-            System.out.println(m.getClass());
-            System.out.println("emf.getPersistenceUnitUtil().isLoaded(m) = " + emf.getPersistenceUnitUtil().isLoaded(m));
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
             tx.commit();
         } catch (Exception e) {
