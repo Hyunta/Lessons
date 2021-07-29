4eb9bb9ac16f485c2795f199b1159808200c9c6f
Hyunta <mohai2618@naver.com>
2021-07-29 18:22:02 +0900

    feat: Add Cascade

--- Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
@@ -23,11 +24,11 @@ public class Order extends BaseEntity{
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @OneToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
