6c9847e08fc104948bb879388e73d3bb60382866
Hyunta <mohai2618@naver.com>
2021-08-04 17:40:50 +0900

    값 타입의 비교

--- Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -3,12 +3,18 @@ package hellojpa;
 public class ValueMain {
     public static void main(String[] args) {
 
-        Integer a = new Integer(10);
-        Integer b = a;
+        int a = 10;
+        int b = 10;
 
-        a = 20;
+        System.out.println("a == b : " + (a == b));
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        Address address1 = new Address("city", "street", "zipcode");
+        Address address2 = new Address("city", "street", "zipcode");
+
+        System.out.println("address1 == address2 : " + (address1 == address2));
+        System.out.println("address1 equals address2 : " + (address1.equals(address2)));
+
+
+
     }
 }
