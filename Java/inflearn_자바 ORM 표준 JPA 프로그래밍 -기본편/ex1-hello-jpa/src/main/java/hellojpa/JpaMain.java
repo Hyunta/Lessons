e5cd72965a2ca9ba7ab18b052117a67704fc522f
Hyunta <mohai2618@naver.com>
2021-08-05 16:05:49 +0900

    객체지향 쿼리 언어

--- Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Java/inflearn_자바 ORM 표준 JPA 프로그래밍 -기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
@@ -19,33 +22,17 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homecity1","street","10000"));
+            //Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
+            //루트 클래스 (조회를 시작할 클래스)
+            Root<Member> m = query.from(Member.class);
 
-            member.getAddressHistory().add(new AddressEntity("old1", "streetOld", "9999"));
-            member.getAddressHistory().add(new AddressEntity("old2", "streetOld2", "9999"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("====================START==================");
-            Member findMember = em.find(Member.class, member.getId());
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(),a.getZipcode()));
-//
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-//
-//            findMember.getAddressHistory().remove(new AddressEntity("old1", "streetOld", "9999"));
-//            findMember.getAddressHistory().add(new AddressEntity("newCity1", "streetOld", "9999"));
-
+            //쿼리 생성 CriteriaQuery<Member> cq =
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+            List<Member> resultList = em.createQuery(cq)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
