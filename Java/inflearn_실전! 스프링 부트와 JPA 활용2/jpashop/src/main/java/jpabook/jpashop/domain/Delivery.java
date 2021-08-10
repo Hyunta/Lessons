230c9fba2f541dbd6555c4c5d0ea04f2eadad9c0
Hyunta <mohai2618@naver.com>
2021-08-10 16:52:43 +0900

    feat: Add Domains

---
+++ Java/inflearn_실전! 스프링 부트와 JPA 활용2/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // Default : Ordinal 1,2,3 사용X
+    private DeliveryStatus status;
+
+}
