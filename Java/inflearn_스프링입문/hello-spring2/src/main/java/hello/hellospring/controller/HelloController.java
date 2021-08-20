6020136c286800d14bf303fa58ce25def41ed8a9
Hyunta <mohai2618@naver.com>
2021-08-20 17:59:01 +0900

    feat(Spring2): add response templates and API

--- Java/inflearn_스프링입문/hello-spring2/src/main/java/hello/hellospring/controller/HelloController.java
+++ Java/inflearn_스프링입문/hello-spring2/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,12 @@
 package hello.hellospring.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,6 +15,33 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Hyuntae!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @Getter
+    @Setter
+    static class Hello {
+        private String name;
+
     }
 
 }
