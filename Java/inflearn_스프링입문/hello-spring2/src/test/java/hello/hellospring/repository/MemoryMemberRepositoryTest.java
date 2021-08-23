71b1772a2d009d65b8b6bddcf66db3d503a7713f
Hyunta <mohai2618@naver.com>
2021-08-23 15:11:27 +0900

    feat(Spring2): Add MemberService

--- Java/inflearn_스프링입문/hello-spring2/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ Java/inflearn_스프링입문/hello-spring2/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-
 class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
