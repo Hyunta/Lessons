5ccd8611200c67c2804002db4d44e90fd144656f
Hyunta <mohai2618@naver.com>
2021-08-23 16:52:27 +0900

    feat(Spring2): Add SpringBean

--- Java/inflearn_스프링입문/hello-spring2/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ Java/inflearn_스프링입문/hello-spring2/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,9 +1,7 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
