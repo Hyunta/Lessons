fcec9285651368094e34de7f5b3b2d0bf651c8fa
Hyunta <mohai2618@naver.com>
2021-08-24 16:16:04 +0900

    feat(Spring2): Add SpringIntegrationTest

---
+++ Java/inflearn_스프링입문/hello-spring2/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,59 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    void 중복회원예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("springTest");
+
+        Member member2 = new Member();
+        member2.setName("springTest");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException illegalStateException = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(illegalStateException.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        try {
+//            memberService.join(member2);
+//            fail("예외가 발생해야 합니다.");
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+
+        //then
+
+    }
+}
