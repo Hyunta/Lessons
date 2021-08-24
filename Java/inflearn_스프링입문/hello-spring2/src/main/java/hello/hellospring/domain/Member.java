b6d4711c038af110bac3d240138c80d4386480a1
Hyunta <mohai2618@naver.com>
2021-08-24 17:05:04 +0900

    feat(Spring2): Add JpaMemberRepository

--- Java/inflearn_스프링입문/hello-spring2/src/main/java/hello/hellospring/domain/Member.java
+++ Java/inflearn_스프링입문/hello-spring2/src/main/java/hello/hellospring/domain/Member.java
@@ -3,9 +3,19 @@ package hello.hellospring.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Getter
 @Setter
 public class Member {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "username")
     private String name;
 }
--- Java/inflearn_스프링입문/hello-spring2/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ Java/inflearn_스프링입문/hello-spring2/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -2,7 +2,6 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
