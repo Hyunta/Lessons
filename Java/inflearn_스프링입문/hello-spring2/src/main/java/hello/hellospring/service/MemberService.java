890b5c859002a4d03dac241c43ed2364424a8b03
Hyunta <mohai2618@naver.com>
2021-08-24 17:48:36 +0900

    feat(Spring2): Add timeStamp

--- Java/inflearn_스프링입문/hello-spring2/src/main/java/hello/hellospring/service/MemberService.java
+++ Java/inflearn_스프링입문/hello-spring2/src/main/java/hello/hellospring/service/MemberService.java
@@ -20,10 +20,17 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
+        long start = System.currentTimeMillis();
         //같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member); // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
