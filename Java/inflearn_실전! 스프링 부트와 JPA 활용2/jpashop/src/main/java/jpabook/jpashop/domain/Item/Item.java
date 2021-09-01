0fdeab2a484c47622468b975b72ebbfb2d5b4b1c
Hyunta <mohai2618@naver.com>
2021-09-01 17:51:50 +0900

    feat(jpaShop) : Add addStock, removeStock in ItemEntity

--- Java/inflearn_실전! 스프링 부트와 JPA 활용2/jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ Java/inflearn_실전! 스프링 부트와 JPA 활용2/jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.Item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,19 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * 비지니스 로직
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
