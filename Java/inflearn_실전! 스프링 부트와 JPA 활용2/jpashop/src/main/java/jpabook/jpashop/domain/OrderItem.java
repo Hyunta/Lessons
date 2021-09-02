c90a7b661480c40af2ba03e4bb83f9c1b7a83351
Hyunta <mohai2618@naver.com>
2021-09-02 16:04:28 +0900

    feat(jpaShop) : Add Constructor, cancel logic

--- Java/inflearn_실전! 스프링 부트와 JPA 활용2/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ Java/inflearn_실전! 스프링 부트와 JPA 활용2/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -9,7 +9,8 @@ import javax.persistence.*;
 import static javax.persistence.FetchType.*;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class OrderItem {
 
     @Id
@@ -26,8 +27,26 @@ public class OrderItem {
     private Order order;
 
     private int orderPrice;
-
     private int count;
 
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //비지니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 
 }
