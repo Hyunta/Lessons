3448878b3b877b73d451629bddfb12b2459baf2d
Hyunta <mohai2618@naver.com>
2021-09-14 18:24:56 +0900

    feat(jpaShop) : add SimpleOrderV4

--- Java/inflearn_실전! 스프링 부트와 JPA 활용2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ Java/inflearn_실전! 스프링 부트와 JPA 활용2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.OrderSimpleQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,6 +55,11 @@ public class OrderSimpleApiController {
                 .map(o -> new SimpleOrderDto(o))
                 .collect(Collectors.toList());
         return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderRepository.findOrderDtos();
     }
 
 
