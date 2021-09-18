360f73861a469c8769737d59c271941bbcc225bc
Hyunta <mohai2618@naver.com>
2021-09-19 03:00:42 +0900

    feat(core): Add MemberServiceTest

---
+++ Java/inflearn_스프링 핵심원리/core2/src/main/java/hello/core2/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.core2;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+import hello.core2.member.MemberService;
+import hello.core2.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
