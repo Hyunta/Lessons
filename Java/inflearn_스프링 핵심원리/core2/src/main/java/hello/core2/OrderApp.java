6b53e515d5d9ad8b85e238e926bd13276c45bcc3
Hyunta <mohai2618@naver.com>
2021-09-20 01:44:57 +0900

    feat(core): Add OrderApp

---
+++ Java/inflearn_스프링 핵심원리/core2/src/main/java/hello/core2/OrderApp.java
@@ -0,0 +1,23 @@
+package hello.core2;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+import hello.core2.member.MemberService;
+import hello.core2.member.MemberServiceImpl;
+import hello.core2.order.Order;
+import hello.core2.order.OrderService;
+import hello.core2.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "Kim", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+    }
+}
