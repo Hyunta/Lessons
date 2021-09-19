d48aa873124a2ce040faea46c4482e241ed01872
Hyunta <mohai2618@naver.com>
2021-09-20 01:48:34 +0900

    feat(core): Add OrderServiceTest

---
+++ Java/inflearn_스프링 핵심원리/core2/src/test/java/hello/core2/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core2.order;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+import hello.core2.member.MemberService;
+import hello.core2.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "Kim", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
