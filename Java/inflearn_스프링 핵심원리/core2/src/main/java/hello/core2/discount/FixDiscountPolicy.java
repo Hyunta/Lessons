f768a42b600385247efb6b0940ad532ecc110c1c
Hyunta <mohai2618@naver.com>
2021-09-20 01:33:58 +0900

    feat(core): Add FixDiscountPolicy

---
+++ Java/inflearn_스프링 핵심원리/core2/src/main/java/hello/core2/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core2.discount;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
