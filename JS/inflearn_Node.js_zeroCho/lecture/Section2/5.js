5ed6ee16925562850204264a6f87075e2a5fc757
Hyunta <mohai2618@naver.com>
2021-10-02 23:53:19 +0900

    2.2.5 구조분해 할당

---
+++ JS/inflearn_Node.js_zeroCho/lecture/Section2/5.js
@@ -0,0 +1,22 @@
+const example = {a:123, b:{c:234, d: 345}}
+/**
+ * 기존 문법
+ */
+// const a = example.a;
+// const d = example.b.d;
+
+/**
+ * 구조분해 할당
+ */
+const {a, b: {d}} = example;
+console.log(a);
+console.log(d);
+
+// arr = [1,2,3,4,5]
+// const x = arr[0];
+// const y = arr[1];
+// const z = arr[4];
+
+const [x,,,y,z]= [1,2,3,4,5]
+
+console.log(x,y,z)
