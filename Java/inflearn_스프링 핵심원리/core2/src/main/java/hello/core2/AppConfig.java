75747f2be7f28e6996e97e2be01d159d9a0b7612
Hyunta <mohai2618@naver.com>
2021-10-12 17:10:37 +0900

    feat(core): Refactor AppConfig

--- Java/inflearn_스프링 핵심원리/core2/src/main/java/hello/core2/AppConfig.java
+++ Java/inflearn_스프링 핵심원리/core2/src/main/java/hello/core2/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core2;
 
+import hello.core2.discount.DiscountPolicy;
 import hello.core2.discount.FixDiscountPolicy;
+import hello.core2.member.MemberRepository;
 import hello.core2.member.MemberService;
 import hello.core2.member.MemberServiceImpl;
 import hello.core2.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import hello.core2.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
