0a0c93cd382e45e1c6b7fcb0a86239bb913843a1
Hyunta <mohai2618@naver.com>
2021-10-14 18:24:30 +0900

    feat(core): Use Xml as Configuration

---
+++ Java/inflearn_스프링 핵심원리/core2/src/test/java/hello/core2/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package hello.core2.xml;
+
+import hello.core2.member.Member;
+import hello.core2.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
