492fa9e80f6aa0210c097f1799ca1832b4ab2879
Hyunta <mohai2618@naver.com>
2021-10-18 18:13:13 +0900

    feat(core): 빈 자동생성 vs 수동생성

--- Java/inflearn_스프링 핵심원리/core2/src/main/java/hello/core2/AutoAppConfig.java
+++ Java/inflearn_스프링 핵심원리/core2/src/main/java/hello/core2/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core2;
 
+import hello.core2.member.MemberRepository;
+import hello.core2.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,5 +12,4 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
 }
