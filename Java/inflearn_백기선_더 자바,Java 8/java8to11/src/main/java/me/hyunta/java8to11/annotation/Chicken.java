9ddc710d02f6601e58d972fb692c939252c8c07b
Hyunta <mohai2618@naver.com>
2022-01-05 18:17:43 +0900

    220105 더자바8 Annotation의 변화

---
+++ Java/inflearn_백기선_더 자바,Java 8/java8to11/src/main/java/me/hyunta/java8to11/annotation/Chicken.java
@@ -0,0 +1,11 @@
+package me.hyunta.java8to11.annotation;
+
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
+@Repeatable(ChickenContainer.class)
+public @interface Chicken {
+    String value();
+}
