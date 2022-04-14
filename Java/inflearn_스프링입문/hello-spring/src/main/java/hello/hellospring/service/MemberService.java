87ab873260e8ce66353e83876f508e4a98692287
Hyunta <mohai2618@gmail.com>
2022-04-14 17:45:38 +0900

    feat: SpringConfig에서 직접 스프링 빈 등록하기

--- Java/inflearn_스프링입문/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ Java/inflearn_스프링입문/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,15 +4,12 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
