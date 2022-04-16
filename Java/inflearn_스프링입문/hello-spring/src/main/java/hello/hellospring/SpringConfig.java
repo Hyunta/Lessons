54dfb41a9c47cbd827c6d92c3acc7d559e8beaa1
Hyunta <mohai2618@gmail.com>
2022-04-16 18:25:43 +0900

    feat: JdbcTemplate 을 이용하여 저장

--- Java/inflearn_스프링입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Java/inflearn_스프링입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,6 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import javax.sql.DataSource;
@@ -26,6 +26,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
