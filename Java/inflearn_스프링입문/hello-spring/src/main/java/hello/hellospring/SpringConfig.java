88f220892feea002b9bb59fe7ee43ebd6dfa64e5
Hyunta <mohai2618@gmail.com>
2022-04-17 17:06:10 +0900

    feat: Aop 적용해보기

--- Java/inflearn_스프링입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Java/inflearn_스프링입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -2,6 +2,7 @@ package hello.hellospring;
 
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +24,7 @@ public class SpringConfig {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
