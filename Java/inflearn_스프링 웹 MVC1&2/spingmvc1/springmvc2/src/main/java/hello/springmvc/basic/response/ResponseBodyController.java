5bf1da412c2b3ad00c1274504f9580075c9a26ae
Hyunta <mohai2618@gmail.com>
2022-05-01 11:35:54 +0900

    ResponseBody 정리

--- Java/inflearn_스프링 웹 MVC1&2/spingmvc1/springmvc2/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ Java/inflearn_스프링 웹 MVC1&2/spingmvc1/springmvc2/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -6,9 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
