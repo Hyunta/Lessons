9e173eb5d3ed2b3c5e7ae21d98ebe1430b7c90c2
Hyunta <mohai2618@gmail.com>
2022-05-28 14:23:15 +0900

    로그인 처리1 : 쿠키와 세션

--- Java/inflearn_스프링 웹 MVC1&2/springmvc2/login2/src/main/java/hello/login/TestDataInit.java
+++ Java/inflearn_스프링 웹 MVC1&2/springmvc2/login2/src/main/java/hello/login/TestDataInit.java
@@ -2,7 +2,10 @@ package hello.login;
 
 import hello.login.domain.item.Item;
 import hello.login.domain.item.ItemRepository;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.support.SpringFactoriesLoader;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -12,6 +15,7 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
@@ -20,6 +24,12 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
-    }
 
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+
+        memberRepository.save(member);
+    }
 }
