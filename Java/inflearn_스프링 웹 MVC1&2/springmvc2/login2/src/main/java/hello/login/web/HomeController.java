83af0f283da9b5a82512d58f7f46ac4fcb6cb1f6
Hyunta <mohai2618@gmail.com>
2022-05-28 15:45:38 +0900

    로그인 처리2 : 인터셉터 & ArgumentResolver 활용

--- Java/inflearn_스프링 웹 MVC1&2/springmvc2/login2/src/main/java/hello/login/web/HomeController.java
+++ Java/inflearn_스프링 웹 MVC1&2/springmvc2/login2/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -72,7 +73,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member,
                                     Model model) {
         if (member == null) {
@@ -80,6 +81,16 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
