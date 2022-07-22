3b9b72ab987302a9064701a3a07cb1de7329fc3b
Hyunta <mohai2618@naver.com>
2022-07-22 10:14:57 +0900

    스프링 DB 2편: SpringDataJpa 적용

--- Java/inflearn_스프링 DB/2편/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Java/inflearn_스프링 DB/2편/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,7 @@
 package hello.itemservice;
 
 import hello.itemservice.config.JpaConfig;
+import hello.itemservice.config.SpringDataJpaConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +16,8 @@ import org.springframework.context.annotation.Profile;
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MyBatisConfig.class)
-@Import(JpaConfig.class)
+//@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
