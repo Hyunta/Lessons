fc33670fff1595b9a33abc990efe27b56d6f53a0
Hyunta <mohai2618@gmail.com>
2022-11-12 16:15:44 +0900

    feat: Naive JpaRepository test

---
+++ JPA/실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
@@ -0,0 +1,44 @@
+package study.datajpa.repository;
+
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Team;
+
+@Repository
+public class TeamRepository {
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    public TeamRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class)
+                .getResultList();
+    }
+
+    public Optional<Team> findById(Long id) {
+        Team team = em.find(Team.class, id);
+        return Optional.ofNullable(team);
+    }
+
+    public long count() {
+        return em.createQuery("select  count(t) from Team t", Long.class)
+                .getSingleResult();
+    }
+
+}
