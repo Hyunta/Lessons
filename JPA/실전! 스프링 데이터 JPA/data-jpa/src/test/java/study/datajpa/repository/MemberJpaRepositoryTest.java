18b4ed0461474c74f2775dd29800078f2311921a
Hyunta <mohai2618@gmail.com>
2022-11-14 00:16:09 +0900

    test: 쿼리메서드 기능 확인

--- JPA/실전! 스프링 데이터 JPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ JPA/실전! 스프링 데이터 JPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -58,4 +58,18 @@ class MemberJpaRepositoryTest {
         long deletedCount = memberJpaRepository.count();
         assertThat(deletedCount).isEqualTo(0);
     }
+
+    @Test
+    void findByUsernameAndGetGreaterThen() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberJpaRepository.save(m1);
+        memberJpaRepository.save(m2);
+
+        List<Member> result = memberJpaRepository.findByUsernameAndGetGreaterThen("AAA", 15);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
 }
