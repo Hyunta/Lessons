bfac595fa8d1627a3e1a89a254f4b9118b1bdb25
Hyunta <mohai2618@gmail.com>
2022-12-08 20:32:56 +0900

    feat: @Aspect를 이용한 AOP 구현

--- Java/스프링 핵심원리-고급편/aop/src/test/java/hello/aop/AopTest.java
+++ Java/스프링 핵심원리-고급편/aop/src/test/java/hello/aop/AopTest.java
@@ -4,14 +4,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
     @Autowired
     OrderService orderService;
@@ -35,3 +38,4 @@ public class AopTest {
                 .isInstanceOf(IllegalStateException.class);
     }
 }
+
