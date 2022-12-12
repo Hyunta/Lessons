d28850c05a63faa126045db27594d4d1920a7fec
Hyunta <mohai2618@gmail.com>
2022-12-12 14:53:49 +0900

    feat: 다양한 Advice 활용

--- Java/스프링 핵심원리-고급편/aop/src/test/java/hello/aop/AopTest.java
+++ Java/스프링 핵심원리-고급편/aop/src/test/java/hello/aop/AopTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -18,7 +18,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import({AspectV6Advice.class})
 public class AopTest {
     @Autowired
     OrderService orderService;
