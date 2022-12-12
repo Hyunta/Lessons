f80d69686b3a7398f661bfe8e62399e16467ac0a
Hyunta <mohai2618@gmail.com>
2022-12-12 14:31:36 +0900

    feat: 내부 클래스를 활용하여 우선순위 부여

--- Java/스프링 핵심원리-고급편/aop/src/test/java/hello/aop/AopTest.java
+++ Java/스프링 핵심원리-고급편/aop/src/test/java/hello/aop/AopTest.java
@@ -4,9 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV2;
-import hello.aop.order.aop.AspectV3;
-import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -19,7 +17,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
     @Autowired
     OrderService orderService;
