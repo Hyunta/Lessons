9872020df2cd4d058b34ce5bf4b144cbf2f2e9c4
Hyunta <mohai2618@gmail.com>
2022-12-13 21:44:17 +0900

    feat: 다양한 Advice 활용 시점

--- Java/스프링 핵심원리-고급편/aop/src/main/java/hello/aop/order/aop/AspectV6Advice.java
+++ Java/스프링 핵심원리-고급편/aop/src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -2,9 +2,11 @@ package hello.aop.order.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -12,24 +14,24 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class AspectV6Advice {
 
-//    @Around("hello.aop.order.aop.PointCuts.orderAndService()")
-//    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
-//        try {
-//            //@Before
-//            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
-//            Object result = joinPoint.proceed();
-//            //@AfterReturning
-//            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
-//            return result;
-//        } catch (Exception e) {
-//            //@AfterThrowing
-//            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
-//            throw e;
-//        } finally {
-//            //@After
-//            log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
-//        }
-//    }
+    @Around("hello.aop.order.aop.PointCuts.orderAndService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            //@Before
+            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            //@AfterReturning
+            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+            return result;
+        } catch (Exception e) {
+            //@AfterThrowing
+            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+            throw e;
+        } finally {
+            //@After
+            log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
+        }
+    }
 
     @Before("hello.aop.order.aop.PointCuts.orderAndService()")
     public void doBefore(JoinPoint joinPoint) {
