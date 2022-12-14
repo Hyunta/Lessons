9c4db30a28e1a0eb3445bcac2a79fe6d34ec7031
Hyunta <mohai2618@gmail.com>
2022-12-14 14:50:25 +0900

    feat: 포인트컷 작동원리 예제 생성

---
+++ Java/스프링 핵심원리-고급편/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -0,0 +1,25 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+@Slf4j
+public class ExecutionTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void printMethod() {
+        log.info("helloMethod = {}", helloMethod);
+    }
+}
