110a01d6be2612ea87258699bd0d5924c966395f
Hyunta <mohai2618@gmail.com>
2022-12-19 18:00:48 +0900

    feat: 가장 정확한 포인트컷

--- Java/스프링 핵심원리-고급편/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ Java/스프링 핵심원리-고급편/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -3,6 +3,7 @@ package hello.aop.pointcut;
 import hello.aop.member.MemberServiceImpl;
 import java.lang.reflect.Method;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
@@ -20,6 +21,13 @@ public class ExecutionTest {
 
     @Test
     void printMethod() {
+        //public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
         log.info("helloMethod = {}", helloMethod);
+    }
+
+    @Test
+    void exactMatch() {
+        pointcut.setExpression("execution(public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String))");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 }
