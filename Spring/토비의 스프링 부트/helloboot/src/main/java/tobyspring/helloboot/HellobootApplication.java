2446bf168b1d60ed5ea1f1cc1da6cb45997fb09b
Hyunta <mohai2618@gmail.com>
2023-01-20 09:41:44 +0900

    init springboot project

---
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,11 @@
+package tobyspring.helloboot;
+
+public class HellobootApplication
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Containerless Standalone Application");
+    }
+
+}
