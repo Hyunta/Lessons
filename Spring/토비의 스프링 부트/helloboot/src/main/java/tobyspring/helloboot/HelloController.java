6df661dfd736ba9bf36b2aab238cd7312085e717
Hyunta <mohai2618@gmail.com>
2023-01-22 12:33:03 +0900

    feat: 비지니스 로직을 service로 분리

--- Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,8 +1,11 @@
 package tobyspring.helloboot;
 
+import java.util.Objects;
+
 public class HelloController {
 
-  public String hello(String name) {
-    return "Hello " + name;
-  }
+    public String hello(String name) {
+        SimpleHelloService helloService = new SimpleHelloService();
+        return helloService.sayHello(Objects.requireNonNull(name));
+    }
 }
