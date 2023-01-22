677ae10f5211fac5550acb20f0368a32d989c5de
Hyunta <mohai2618@gmail.com>
2023-01-22 12:58:47 +0900

    feat: 애노테이션을 이용한 bean 등록

--- Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,23 +3,40 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class HellobootApplication {
 
-    public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh();
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
 
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext))
-                    .addMapping("/*");
-        });
-        webServer.start();
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                                    new DispatcherServlet(this))
+                            .addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+        applicationContext.register(HellobootApplication.class);
+        applicationContext.refresh();
     }
 }
