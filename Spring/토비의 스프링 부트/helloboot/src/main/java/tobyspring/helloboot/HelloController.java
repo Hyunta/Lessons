39530df218df074c2ebf3cdb2842537dff1145ac
Hyunta <mohai2618@gmail.com>
2023-01-22 12:51:09 +0900

    feat: dispatcherServlet 등록

--- Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,11 @@
 package tobyspring.helloboot;
 
 import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,6 +14,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
