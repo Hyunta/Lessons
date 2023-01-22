ed47c7ea0ffd89e5800e430ae008a4209b9da867
Hyunta <mohai2618@gmail.com>
2023-01-22 12:27:14 +0900

    feat: GenericApplicationContext를 이용한 빈 등록

--- Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,38 +8,42 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 public class HellobootApplication {
 
-  public static void main(String[] args) {
-    ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-    WebServer webServer = serverFactory.getWebServer(servletContext -> {
-      HelloController helloController = new HelloController();
+    public static void main(String[] args) {
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
 
-      servletContext.addServlet("frontController", new HttpServlet() {
-        @Override
-        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-          // 인증, 보안, 다국어, 공통 기능 처리
-          if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-            String name = req.getParameter("name");
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
 
-            String ret = helloController.hello(name);
+            servletContext.addServlet("frontController", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp)
+                        throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능 처리
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
 
-            resp.setStatus(HttpStatus.OK.value());
-            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-            resp.getWriter().println(ret);
-          } else if (req.getRequestURI().equals("/user")) {
-            //
-          } else {
-            resp.setStatus(HttpStatus.NOT_FOUND.value());
-          }
-        }
-      }).addMapping("/*");
-    });
-    webServer.start();
-  }
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
+                        String ret = helloController.hello(name);
+
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(ret);
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+                }
+            }).addMapping("/*");
+        });
+        webServer.start();
+    }
 }
