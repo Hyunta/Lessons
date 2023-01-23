f89f4071dca1b09a470b163834944b56ffc9a617
Hyunta <mohai2618@gmail.com>
2023-01-23 13:30:42 +0900

    feat: 빈을 이용한 DispatcherServlet 주입

--- Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,20 +1,30 @@
 package tobyspring.helloboot;
 
 import java.util.Objects;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloController implements ApplicationContextAware {
 
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("applicationContext = " + applicationContext);
     }
 }
