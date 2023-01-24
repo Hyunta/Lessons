078fe1c51bc0257a850f21efb7b11025122c40ff
Hyunta <mohai2618@gmail.com>
2023-01-24 17:31:23 +0900

    test: TestRestTemplate을 이용한 테스트

---
+++ Spring/토비의 스프링 부트/helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,25 @@
+package tobyspring.helloboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> response =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        assertThat(response.getBody()).isEqualTo("Hello Spring");
+    }
+}
