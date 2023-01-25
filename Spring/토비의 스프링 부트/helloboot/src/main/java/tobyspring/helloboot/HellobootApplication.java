aac23cfe19a1fa5c2a18493766dbc86bbb71e6d0
Hyunta <mohai2618@gmail.com>
2023-01-25 12:32:57 +0900

    feat: 인프라 빈 구성 정보 분리

--- Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,6 +1,7 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
+import tobyspring.config.MySpringBootAnnotation;
 
 @MySpringBootAnnotation
 public class HellobootApplication {
