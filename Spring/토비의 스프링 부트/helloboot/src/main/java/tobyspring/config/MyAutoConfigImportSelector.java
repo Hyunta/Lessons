e24b293e112f2a9b2765d060d345b1a3a7ffa4aa
Hyunta <mohai2618@gmail.com>
2023-01-25 15:34:27 +0900

    feat: autoConfiguration 애노테이션 적용

--- Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,25 @@
 package tobyspring.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
 
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "tobyspring.config.autoconfig.DispatcherServletConfig",
-                "tobyspring.config.autoconfig.TomcatWebServerConfig",
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader)
+                .forEach(autoConfigs::add);
+        return autoConfigs.toArray(new String[0]);
     }
 }
