294e4ce85dc366620bdc4c0fc66f46d770c87892
Hyunta <mohai2618@gmail.com>
2023-01-28 23:32:31 +0900

    feat: 환경 변수 주입받는 설정 추가

--- Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -11,11 +12,14 @@ import tobyspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath("/app");
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 }
