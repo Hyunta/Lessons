2e90350d38f405417ae29b9b5760b45ce231731b
Hyunta <mohai2618@gmail.com>
2023-01-29 19:08:38 +0900

    feat: properties 클래스 분리

--- Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ Spring/토비의 스프링 부트/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,6 +1,5 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -12,14 +11,14 @@ import tobyspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
-    @Value("${contextPath}")
-    String contextPath;
-
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(this.contextPath);
+
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+
         return factory;
     }
 }
