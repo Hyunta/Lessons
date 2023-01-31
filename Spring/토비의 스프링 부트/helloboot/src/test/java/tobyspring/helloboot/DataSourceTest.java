dd4d54af0f2d23b9b7088b483e8c8d1944d4eb73
Hyunta <mohai2618@gmail.com>
2023-01-31 18:36:56 +0900

    feat: JdbcTemplate과 JdbcTransactionManager 구성

--- Spring/토비의 스프링 부트/helloboot/src/test/java/tobyspring/helloboot/DataSourceTest.java
+++ Spring/토비의 스프링 부트/helloboot/src/test/java/tobyspring/helloboot/DataSourceTest.java
@@ -5,15 +5,9 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = HellobootApplication.class)
-@TestPropertySource("classpath:/application.properties")
+@HelloBootTest
 class DataSourceTest {
 
     @Autowired
---
+++ Spring/토비의 스프링 부트/helloboot/src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,29 @@
+package tobyspring.helloboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HelloBootTest
+public class JdbcTemplateTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void insertAndQuery() {
+        jdbcTemplate.update("insert into hello values(?, ?)", "Toby", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)", "Spring", 1);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        assertThat(count).isEqualTo(2);
+    }
+}
