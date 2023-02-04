6acf71ae0444b8694b22bf356757497b1a5011cd
Hyunta <mohai2618@gmail.com>
2023-02-04 22:17:03 +0900

    feat: using springCloudGateway filter as java

--- MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,9 +1,12 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
@@ -13,4 +16,9 @@ public class FirstServiceController {
         return "Welcome to the First Service";
     }
 
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service";
+    }
 }
