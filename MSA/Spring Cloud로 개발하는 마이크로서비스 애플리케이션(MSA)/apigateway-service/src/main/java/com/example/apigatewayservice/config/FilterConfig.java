172b6f4f8fed7590689834174c7d6d98fbc06768
Hyunta <mohai2618@gmail.com>
2023-02-05 22:20:40 +0900

    feat: add filter using yaml file

--- MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -5,10 +5,11 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Deprecated
+//@Configuration
 public class FilterConfig {
 
-    @Bean
+//    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
