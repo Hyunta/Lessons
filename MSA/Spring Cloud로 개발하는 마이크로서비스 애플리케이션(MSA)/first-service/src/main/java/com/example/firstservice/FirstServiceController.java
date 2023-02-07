b7a9414630a5f27ffd6c69e429309740ab38ecd7
Hyunta <mohai2618@gmail.com>
2023-02-07 15:38:34 +0900

    feat: port 번호를 랜덤하게 부여하여 로드밸런싱

--- MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,6 +1,8 @@
 package com.example.firstservice;
 
+import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+
+    Environment env;
+
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +31,9 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is message from First Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server Port = {}", request.getServerPort());
+        return String.format("Hi, there. This is message from First Service on Port %s",
+                env.getProperty("local.server.port"));
     }
 }
