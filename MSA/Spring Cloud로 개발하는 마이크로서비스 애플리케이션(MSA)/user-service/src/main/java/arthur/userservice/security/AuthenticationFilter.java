58049f3ad05f9c199a2af65aa80c7c2d96285e83
Hyunta <mohai2618@gmail.com>
2023-02-21 15:40:55 +0900

    feat: 로그인 성공시 로그를 작성하도록 구현

--- MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/user-service/src/main/java/arthur/userservice/security/AuthenticationFilter.java
+++ MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/user-service/src/main/java/arthur/userservice/security/AuthenticationFilter.java
@@ -5,14 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
@@ -33,9 +35,9 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
-//        super.successfulAuthentication(request, response, chain, authResult);
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult) {
+        log.debug(((User) authResult.getPrincipal()).getUsername());
     }
 }
