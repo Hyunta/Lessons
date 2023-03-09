d53565fc17358b99e017d857d73ce94c9eec889d
ht <ht@wemakeprice.com>
2023-03-09 17:42:57 +0900

    check

--- MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ MSA/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -20,7 +20,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
     private final Environment env;
 
     public AuthorizationHeaderFilter(Environment env) {
-        super(AuthorizationHeaderFilter.Config.class);
+        super(Config.class);
         this.env = env;
     }
 
