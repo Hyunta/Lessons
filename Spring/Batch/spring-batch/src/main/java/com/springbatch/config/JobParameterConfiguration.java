ffc7d97fddf988b81ee32dfe26d82fba011175eb
Hyunta <mohai2618@gmail.com>
2023-03-13 15:43:20 +0900

    feat: JobParameter 받아오기

--- Spring/Batch/spring-batch/src/main/java/com/springbatch/config/JobParameterConfiguration.java
+++ Spring/Batch/spring-batch/src/main/java/com/springbatch/config/JobParameterConfiguration.java
@@ -1,8 +1,10 @@
 package com.springbatch.config;
 
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -30,6 +32,15 @@ public class JobParameterConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("jobParameterStep1")
                 .tasklet((contribution, chunkContext) -> {
+
+                    JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
+                    jobParameters.getString("name");
+                    jobParameters.getLong("seq");
+                    jobParameters.getDate("date");
+                    jobParameters.getDouble("age");
+
+                    Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+
                     log.info("jobParameter step1 was executed");
                     return RepeatStatus.FINISHED;
                 })
