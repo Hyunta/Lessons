eb41ca8cc30e324f66d922bbc9abec753942c013
Hyunta <mohai2618@gmail.com>
2023-03-15 11:55:02 +0900

    feat: SpringMvc를 이용한 컨테이너 등록

---
+++ Spring/김영한_스프링 부트/server/src/main/java/hello/container/AppInitV3SpringMvc.java
@@ -0,0 +1,27 @@
+package hello.container;
+
+import hello.spring.HelloConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration.Dynamic;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitV3SpringMvc implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        System.out.println("AppInitV3SpringMvc.onStartup");
+
+        //스프링 컨테이너 생성
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(HelloConfig.class);
+
+        //스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
+        DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+        Dynamic servlet = servletContext.addServlet("dispatcherV3", dispatcher);
+        servlet.addMapping("/");
+
+    }
+}
