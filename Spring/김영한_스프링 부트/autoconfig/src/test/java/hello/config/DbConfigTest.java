15bb9639fdfba07cb82cd65a2b89ef95b5ff02b1
Hyunta <mohai2618@gmail.com>
2023-03-19 23:55:48 +0900

    test: config test

--- Spring/김영한_스프링 부트/autoconfig/src/test/java/hello/config/DbConfigTest.java
+++ Spring/김영한_스프링 부트/autoconfig/src/test/java/hello/config/DbConfigTest.java
@@ -1,7 +1,28 @@
 package hello.config;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.TransactionManager;
 
+@Slf4j
+@SpringBootTest
 class DbConfigTest {
 
+    @Autowired
+    DataSource dataSource;
+    @Autowired
+    TransactionManager transactionManager;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    void checkBean() {
+        log.info("dataSource = {}", dataSource);
+        log.info("transactionManager = {}", transactionManager);
+        log.info("jdbcTemplate = {}", jdbcTemplate);
+    }
 }
