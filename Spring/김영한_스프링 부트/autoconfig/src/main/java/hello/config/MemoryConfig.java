9a44ac118e35d13e94375041dd52b18fc54cdfca
Hyunta <mohai2618@gmail.com>
2023-03-22 16:33:31 +0900

    feat: SpringBoot에서 제공하는 ConditionalOnProperty 사용

--- Spring/김영한_스프링 부트/autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ Spring/김영한_스프링 부트/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -2,10 +2,13 @@ package hello.config;
 
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+//@Conditional(MemoryCondition.class)
+@ConditionalOnProperty(name = "memory", havingValue = "on")
 public class MemoryConfig {
 
     @Bean
